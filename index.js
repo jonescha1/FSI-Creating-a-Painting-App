d5b2953e28603ce11acadadc6d3448b51fcf7405
Chase Jones <chasejones@Chases-MacBook-Pro.local>
2022-01-26 22:16:25 -0500

    BONUS---Added ability to clear current color

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const palette = document.querySelector(".palette");
 let selectedColor = null;
 
 painting.addEventListener("click", function (e) {
-  e.target.style.backgroundColor = selectedColor;
+  if (e.target.style.backgroundColor == selectedColor) {
+    e.target.style.backgroundColor = "white";
+  } else {
+    e.target.style.backgroundColor = selectedColor;
+  }
 });
 
 palette.addEventListener("click", function (e) {
